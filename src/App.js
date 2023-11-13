aaf493309eea89d06250e594fbe9fe181721cc83
SeunSarumi <seunsarumi@gmail.com>
2023-11-13 15:09:57 +0100

    children prop replacements

--- src/App.js
+++ src/App.js
@@ -32,9 +32,13 @@ function App() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}{" "}
-          </p>
+
+          {/* <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p> */}
+
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+
           <div className="buttons">
             <Button bgColor="#7950f2" textColor="#fff" onClick={handlePrevious}>
               <span>👈🏼</span>Previous
@@ -46,6 +50,15 @@ function App() {
         </div>
       )}
     </>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
   );
 }
 
